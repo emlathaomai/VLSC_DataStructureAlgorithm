201d0f30406c2ea9faff56cb517cdcb093490182
vtd <2500115772@vanlangsaigon.edu.vn>
2026-03-30 11:12:36 +0700

    Khoi tao cau truc Buoi 5

--- DSA_Session04_Sorting/Program.cs
+++ DSA_Session04_Sorting/Program.cs
@@ -1,2 +1,86 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+// Võ Thành Đạt - 2500115772
+// Mục tiêu : Học các sử dụng thuật toán sắp xếp cơ bản
+
+class Program {
+    static void Main() {
+        Console.OutputEncoding = System.Text.Encoding.UTF8;
+        
+        //bubble sort
+
+        Console.Write("Nhap so luong phan tu: ");
+        int n = int.Parse(Console.ReadLine()!);
+        int[] arr = new int[n];
+        Console.WriteLine("Nhap cac phan tu:");
+        for (int i = 0; i < n; i++) {
+            Console.Write($"arr[{i}]: ");
+            arr[i] = int.Parse(Console.ReadLine()!);
+        }
+
+        BubbleSort(arr);
+        Console.WriteLine("Mang sau khi sap xep:");
+        PrintArray(arr);
+    }
+
+    static void BubbleSort(int[] arr) {
+        int n = arr.Length;
+        for (int i = 0; i < n - 1; i++) {
+            for (int j = 0; j < n - i - 1; j++) {
+                if (arr[j] > arr[j + 1]) {
+                    int temp = arr[j];
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = temp;
+                }
+            }
+        }
+    }
+
+    static void PrintArray(int[] arr) {
+        foreach (int num in arr) {
+            Console.Write(num + " ");
+        }
+        Console.WriteLine();
+    }
+
+    //insertion sort
+
+    static void InsertionSort(int[] arr)
+{
+    int n = arr.Length;
+    for (int i = 1; i < n; i++)
+    {
+        int key = arr[i];
+        int j = i - 1;
+
+        while (j >= 0 && arr[j] > key)
+        {
+            arr[j + 1] = arr[j];
+            j--;
+        }
+        arr[j + 1] = key;
+    }
+}
+static void Main2() {
+    Console.OutputEncoding = System.Text.Encoding.UTF8;
+    Console.Write("Nhap so luong phan tu: ");
+
+    int n = int.Parse(Console.ReadLine()!);
+    int[] arr = new int[n];
+    Console.WriteLine("Nhap cac phan tu:");
+    for (int i = 0; i < n; i++) {
+        Console.Write($"arr[{i}]: ");
+        arr[i] = int.Parse(Console.ReadLine()!);
+    }
+
+    //BubbleSort(arr);
+    Console.WriteLine("Mang sau khi sap xep:");
+    PrintArray(arr);
+
+    InsertionSort(arr);
+    Console.WriteLine("Mang sau khi sap xep bang Insertion Sort:");
+    PrintArray(arr);
+}
+
+
+}
+
